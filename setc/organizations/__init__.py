f86f2b97c9c2e602e3ebd20efd7ed9f77d1b7e82
Dr. Oliver IHouma Ocubaus <50647470+Everwild1@users.noreply.github.com>
2026-08-15 17:49:40 -0400

    Add canonical SETC organization domain

---
+++ setc/organizations/__init__.py
@@ -0,0 +1,5 @@
+"""Canonical SETC Organizations domain."""
+
+from .models import Organization, OrganizationCapability, OrganizationType, VerificationState
+
+__all__ = ["Organization", "OrganizationCapability", "OrganizationType", "VerificationState"]
